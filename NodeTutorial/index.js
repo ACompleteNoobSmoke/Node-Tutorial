66ed734b0df35f26d62863b4a022e8e03515048c
Omoruyi Omofonmwan <OmoruyiOmofonmwan@yahoo.com>
2020-03-06 19:54:55 -0600

    Made a simple server service

--- NodeTutorial/index.js
+++ NodeTutorial/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const person = require('./person')
 
 const server = http.createServer((req, res) => {
+    /*
     if (req.url === '/') {
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
             if (err) throw err;
@@ -30,6 +31,57 @@ const server = http.createServer((req, res) => {
         });
         res.end(JSON.stringify(users));
     }
+    */
+
+    //Build File Path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    //Extension Of File
+    let extname = path.extname(filePath);
+
+    //Initial Content Type
+    let contentType = 'text/html';
+
+    //Check Ext And Set Content Type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+
+    //Read File
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                //Page Not Found Error
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                })
+            } else {
+                //Some Other Server Error 
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            //Successful Response
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    })
 });
 
 const PORT = process.env.PORT || 5000;
