dc02f000e2496b0524ae821ea95820f32c652d0b
Omoruyi Omofonmwan <OmoruyiOmofonmwan@yahoo.com>
2020-03-06 19:30:56 -0600

    Made a simple server service

--- NodeTutorial/index.js
+++ NodeTutorial/index.js
@@ -1,7 +1,42 @@
-const Person = require('./person');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const person = require('./person')
 
-const person1 = new Person('Adebayo Uwensuyi', 25);
-const person2 = new Person('Ajiboye Uwensuyi', 27);
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(content);
+        })
+    }
 
-person1.greeting();
-person2.greeting();
+    if (req.url === '/about') {
+        fs.readFile(
+            path.join(__dirname, 'public', 'about.html'),
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        )
+    }
+
+    if (req.url === '/api/users') {
+        const users = [p1, p2, p3, p4];
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify(users));
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+const p1 = new person("Uyi", 26);
+const p2 = new person("Osaretin", 24);
+const p3 = new person("Ade", 25);
+const p4 = new person("Boye", 27);
+
+server.listen(PORT, () => console.log(`Server Currently Running On Port ${PORT}`));
