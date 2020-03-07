e6b4aaeefc0737aee9672c6942ca545970dbcbb1
Omoruyi Omofonmwan <OmoruyiOmofonmwan@yahoo.com>
2020-03-06 18:09:16 -0600

    Node Tutorial Project

---
+++ NodeTutorial/index.js
@@ -0,0 +1,7 @@
+const Person = require('./person');
+
+const person1 = new Person('Adebayo Uwensuyi', 25);
+const person2 = new Person('Ajiboye Uwensuyi', 27);
+
+person1.greeting();
+person2.greeting();
